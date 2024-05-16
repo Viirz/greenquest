8e10814b3b5ceca4e754126828f0a6f41633f07e
arthuritokeintjem <keintjemarthurito@gmail.com>
2024-05-16 20:46:20 +0700

    tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,16 +66,18 @@
                     <li><a href="#services" class="page-scroll">Services</a></li>
                     <li><a href="#portfolio" class="page-scroll">Gallery</a></li>
                     <li>
-                        <a href="#testimonials" class="page-scroll">Testimonials</a>
+                        <a href="#faqs" class="page-scroll">FAQs</a>
                     </li>
                     <li><a href="#contact" class="page-scroll">Contact</a></li>
                     @if (Route::has('login'))
 
                         @auth
-                            <a href="{{ url('/dashboard') }}"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                Dashboard
-                            </a>
+                            <li>
+                                <a href="{{ url('/dashboard') }}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                    Dashboard
+                                </a>
+                            </li>
                         @else
                             <li>
                                 <a href="{{ route('login') }}"
@@ -390,52 +392,42 @@
             </div>
         </div>
     </div>
-    <!-- Testimonials Section -->
-    <div id="testimonials" class="text-center">
+    <!-- FAQs Section -->
+    <div id="faqs" class="text-center">
         <div class="overlay">
             <div class="container">
                 <div class="section-title">
-                    <h2>Testimonials</h2>
+                    <h2>FAQs</h2>
                     <hr />
                 </div>
                 <div class="row">
                     <div class="col-md-10 col-md-offset-1">
                         <div id="testimonial" class="owl-carousel owl-theme">
                             <div class="item">
-                                <p>
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante
-                                    facilisis bibendum dolor feugiat at duis sed dapibus leo nec
-                                    ornare diam."
-                                </p>
-                                <p>- John DOE, Parker County, TX</p>
+                                <div class="faqs-question">What is GreenQuest's mission?</div>
+                                <div class="faqs-answer">GreenQuest's mission is to promote environmental sustainability
+                                    through tree
+                                    planting and conservation efforts.</div>
                             </div>
                             <div class="item">
-                                <p>
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante
-                                    facilisis bibendum dolor feugiat at duis sed dapibus leo nec
-                                    ornare diam."
-                                </p>
-                                <p>- Jenny DOE, Parker County, TX</p>
+                                <div class="faqs-question">How can I get involved with GreenQuest?</div>
+                                <div class="faqs-answer">You can get involved with GreenQuest by volunteering for tree
+                                    planting events,
+                                    donating to support our projects, or spreading awareness about our mission.</div>
                             </div>
                             <div class="item">
-                                <p>
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante
-                                    facilisis bibendum dolor feugiat at duis sed dapibus leo nec
-                                    ornare diam."
-                                </p>
-                                <p>- John DOE, Parker County, TX</p>
+                                <div class="faqs-question">How does GreenQuest ensure the sustainability of its
+                                    projects?</div>
+                                <div class="faqs-answer">GreenQuest works closely with local communities and
+                                    environmental experts to
+                                    ensure that tree planting projects are sustainable and have a positive long-term
+                                    impact.</div>
                             </div>
                             <div class="item">
-                                <p>
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante
-                                    facilisis bibendum dolor feugiat at duis sed dapibus leo nec
-                                    ornare diam."
-                                </p>
-                                <p>- John DOE, Parker County, TX</p>
+                                <div class="faqs-question">Is my donation tax-deductible?</div>
+                                <div class="faqs-answer">Yes, donations made to GreenQuest are tax deductible to the
+                                    extent allowed by
+                                    law. Please consult with your tax advisor for more information.</div>
                             </div>
                         </div>
                     </div>
@@ -443,6 +435,7 @@
             </div>
         </div>
     </div>
+
     <!-- Contact Section -->
     <div id="contact" class="text-center">
         <div class="container">
