2c6d1df98cb6c347d63c1c7cf663ef04be1e0c10
IrulIslami <irulislamy29@gmail.com>
2024-05-17 17:01:52 +0700

    edit dashbaord

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,20 +13,14 @@
         <meta property="og:image" content="https://teamtrees.org/images/social-share-earth-astronaut-1-9x1.png" />
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-            crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
         <!-- Google Fonts CSS -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,900&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" href="favicon.ico" />
         <!-- teamtrees CSS -->
         <link href="assets/dashboard/css/main.min.css" rel="stylesheet" />
         <link href="assets/dashboard/css/odometer.css" rel="stylesheet" />
-<<<<<<< HEAD
         <script src="https://github.hubspot.com/odometer/odometer.js"></script>
-        <script src="https://github.hubspot.com/odometer/odometer.js"></script>
-=======
->>>>>>> parent of a48498a (change dashboard euy)
         <title>#teamtrees</title>
 
         <style>
@@ -36,24 +30,6 @@
 
             #link {
                 color: inherit !important;
-            }
-<<<<<<< HEAD
-
-            .map-container {
-                position: relative;
-                text-align: center;
-                background-color: #273654;
-            }
-
-            .label {
-                position: absolute;
-                cursor: pointer;
-                color: black;
-                text-decoration: none;
-            }
-            
-            .news{
-                background-color: #CDE4E4;
             }
 
             .map-container {
@@ -199,11 +175,18 @@
             .team-trees-logo {
                 width: 80%;
             }
-=======
->>>>>>> parent of a48498a (change dashboard euy)
         </style>
-
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $("#mapButton").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $("#map-container").offset().top
+                    }, 1000); // Waktu dalam milidetik untuk animasi gulir (misalnya 1000ms)
+                });
+            });
+        </script>
+
     </head>
 
     <body>
@@ -218,13 +201,7 @@
                         <div class="text-center intro-copy">
                             <h1 class="text-uppercase d-none">#teamtrees</h1>
                             <h1 class="text-center">
-<<<<<<< HEAD
                                 <img class="team-trees-logo" alt="#teamtrees" title="#teamtrees" src="assets/dashboard/images/GQ.png" />
-                                <img class="team-trees-logo" alt="#teamtrees" title="#teamtrees" src="assets/dashboard/images/GQ.png" />
-=======
-                                <img class="team-trees-logo" alt="#teamtrees" title="#teamtrees"
-                                    src="assets/dashboard/images/logo-teamtrees-full.svg" />
->>>>>>> parent of a48498a (change dashboard euy)
                             </h1>
                             <p class="lead mb-2">
                                 Help us plant 20 million trees around the globe by January 1st,
@@ -232,19 +209,11 @@
                             </p>
 
                             <div class="counter-wrap">
-                                <div id="totalTrees" class="counter odometer"> 0</div>
+                                <div id="totalTrees" class="counter odometer"></div>
                                 <div class="measure-wrap">
                                     <span class="text-uppercase letter-spacing" style="top: 4px">Total Donation</span>
-<<<<<<< HEAD
                                     <img class="counter-underline" src="assets/dashboard/images/counter-underline-blue.svg" />
-                                    <img class="counter-underline" src="assets/dashboard/images/counter-underline-blue.svg" />
-=======
-                                    <img class="counter-underline"
-                                        src="assets/dashboard/images/counter-underline-blue.svg" />
->>>>>>> parent of a48498a (change dashboard euy)
                                     <br />
-                                    <a href="https://teamtrees.org/" class="text-uppercase letter-spacing" id="link"
-                                        style="top: 4px">Donate at teamtrees.org!</a>
                                 </div>
                             </div>
                         </div>
@@ -252,12 +221,67 @@
                 </div>
             </div>
         </div>
+        <div class="form-background">
+            
+            
+            <div class="treeDonation">
 
+                <p style="padding: 0.5em 0; font-family: 'PT Sans', sans-serif; font-weight: bold; font-size: 24px;">
+                    Choose your option
+                </p>
+
+                <div class="btns">
+                    <button>1 <br>Trees</button>
+                    <button>5 <br>Trees</button>
+                    <button>20 <br>Trees</button>
+                    <button>50 <br>Trees</button>
+                    <input type="text" placeholder="Other amount">
+                    <input type="submit">
+                </div>
+                
+            </div>
+            <div>
+                    <button class="mapButton" id="mapButton">Go to Map</button>
+                </div>
+        </div>
+
+
+        <div id="map-container" class="map-container">
+            <img src="assets/dashboard/images/petafix.png" alt="Peta Indonesia" style="width: 100%;">
+            <a href="#sumatera-container" class="label" style="top: 39.5%; left: 25.7%;">Sumatera</a>
+            <a href="#kalimantan-container" class="label" style="top: 36.8%; left: 43.2%;">Kalimantan</a>
+            <a href="#sulawesi-container" class="label" style="top: 47.2%; left: 53.6%;">Sulawesi</a>
+            <a href="#papua-container" class="label" style="top: 49.5%; left: 72.6%;">Papua</a>
+            <a href="#jawa-container" class="label" style="top: 61.3%; left: 37.6%;">Jawa</a>
+        </div>
+        <div id="news" class="news">
+            <div id="sumatera-container" class="container">
+                <h2>Sumatera</h2>
+                <p>Informasi tentang Sumatera...</p>
+            </div>
+
+            <div id="kalimantan-container" class="container">
+                <h2>Kalimantan</h2>
+                <p>Informasi tentang Kalimantan...</p>
+            </div>
+
+            <div id="sulawesi-container" class="container">
+                <h2>Sulawesi</h2>
+                <p>Informasi tentang Sulawesi...</p>
+            </div>
+
+            <div id="papua-container" class="container">
+                <h2>Papua</h2>
+                <p>Informasi tentang Papua...</p>
+            </div>
+
+            <div id="jawa-container" class="container">
+                <h2>Jawa</h2>
+                <p>Informasi tentang Jawa...</p>
+            </div>
+        </div>
 
         <script type="text/javascript">
-<<<<<<< HEAD
-            $(document).ready(function() {
-                setInterval(function() {
             $(document).ready(function() {
                 setInterval(function() {
                     $.ajax({
@@ -267,39 +291,15 @@
                             // Assuming the server returns the new total trees count
                             var totalTrees = $('#totalTrees');
                             totalTrees.html(data.sum); // Odometer uses innerHTML instead of text()
-                        success: function(data) {
-                            // Assuming the server returns the new total trees count
-                            var totalTrees = $('#totalTrees');
-                            totalTrees.html(data.sum); // Odometer uses innerHTML instead of text()
                         },
-                        error: function(error) {
                         error: function(error) {
                             console.log(error);
                         }
                     });
                 }, 1000); // 1000 milliseconds = 1 second
-                }, 1000); // 1000 milliseconds = 1 second
-=======
-
-            $(document).ready(function () {
-                setInterval(function () {
-                    $.ajax({
-                        url: '/donations/sum', // replace with your endpoint
-                        type: 'GET',
-                        success: function (data) {
-                            // assuming the server returns the new total trees count
-                            $('#totalTrees').text(data.sum);
-                        },
-                        error: function (error) {
-                            console.log(error);
-                        }
-                    });
-                }, 1000); // 1000 milliseconds = 1 seconds
->>>>>>> parent of a48498a (change dashboard euy)
             });
         </script>
     </body>
 
     </html>
-
 </x-app-layout>
